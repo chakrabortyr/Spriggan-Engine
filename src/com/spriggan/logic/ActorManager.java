1024cb144110b7cbf65eeaf4c970ecfcc668609d
Rudra Chakraborty <chakraborty.rudra.03@gmail.com>
2017-01-14 12:31:46 -0800

    some formatting changes + cleanup

--- src/com/spriggan/logic/ActorManager.java
+++ src/com/spriggan/logic/ActorManager.java
@@ -1,10 +1,6 @@
 /* ActorManager.java
  * Pizzapuncher Productions, 2015.
- * This code is written under what we call the CSL ( Common Sense License ). 
- * We believe that code can be learned from, whether brilliantly or poorly written, and thus we are willing to share.
- * You may use this code for whatever purpose you will, but you should do so in good judgement, at your own risk,
- * and with the understanding that we assume no responsibility and offer no warranty on this code. 
- * We do not even guarantee that it works exactly as advertised.
+ * This code is written under what we call the CSL ( Common Sense License ). We believe that code can be learned from, whether brilliantly or poorly written, and thus we are willing to share. You may use this code for whatever purpose you will, but you should do so in good judgement, at your own risk, and with the understanding that we assume no responsibility and offer no warranty on this code. We do not even guarantee that it works exactly as advertised.
  */
 
 package com.spriggan.logic;
@@ -27,21 +23,22 @@ import com.spriggan.sprite.Placeable;
  * @author chakrabortyr
  */
 public final class ActorManager {
-    private static final Map<String, Group> actors = new HashMap();
-    
-    public void insertActor(SpriteHandle handle) {
-        TextureRegion tex = new TextureRegion(new Texture(Gdx.files.internal(handle.filePath)));
+   private static final Map<String, Group> actors = new HashMap();
 
-        if (handle.dynamic) {
-            SpriteSheet sheet = new SpriteSheet(handle.id, tex, handle.rows, handle.columns, handle.timePerFrame, handle.loop);
-            actors.put(handle.id, sheet);
-        } else {
-            Placeable place = new Placeable(handle.id, tex);
-            actors.put(handle.id, place); // put you in your place. heh.
-        }
-    }
-    
-    public Group getActor(String id) {
-        return actors.get(id);
-    }
+   public void insertActor(SpriteHandle handle) {
+      TextureRegion tex = new TextureRegion(new Texture(Gdx.files.internal(handle.filePath)));
+
+      if (handle.dynamic) {
+         SpriteSheet sheet = new SpriteSheet(handle.id, tex, handle.rows, handle.columns, handle.timePerFrame,
+               handle.loop);
+         actors.put(handle.id, sheet);
+      } else {
+         Placeable place = new Placeable(handle.id, tex);
+         actors.put(handle.id, place); // put you in your place. heh.
+      }
+   }
+
+   public Group getActor(String id) {
+      return actors.get(id);
+   }
 }
--- src/com/spriggan/logic/Entity.java
+++ src/com/spriggan/logic/Entity.java
@@ -1,10 +1,6 @@
-/* Entity.java
+/* ActorManager.java
  * Pizzapuncher Productions, 2015.
- * This code is written under what we call the CSL ( Common Sense License ). 
- * We believe that code can be learned from, whether brilliantly or poorly written, and thus we are willing to share.
- * You may use this code for whatever purpose you will, but you should do so in good judgement, at your own risk,
- * and with the understanding that we assume no responsibility and offer no warranty on this code. 
- * We do not even guarantee that it works exactly as advertised.
+ * This code is written under what we call the CSL ( Common Sense License ). We believe that code can be learned from, whether brilliantly or poorly written, and thus we are willing to share. You may use this code for whatever purpose you will, but you should do so in good judgement, at your own risk, and with the understanding that we assume no responsibility and offer no warranty on this code. We do not even guarantee that it works exactly as advertised.
  */
 
 package com.spriggan.logic;
@@ -19,31 +15,17 @@ import com.spriggan.sprite.SpriteSheet;
  * @author chakrabortyr
  */
 abstract class Entity {
-    //Core External Attributes
-    protected long entityID; // This is always a unique variable! Not to be confused with LegacySprite ID
-    protected String name;  // The name of the Entity, usually unique but we don't assume so.
-    protected SpriteSheet sprite; // Sprite sheet of entity, some will be copies of others, not always unique
+   //Core External Attributes
+   protected String name; // The name of the Entity, usually unique but we don't assume so.
+   protected SpriteSheet spriteBase; // Sprite sheet of entity, some will be copies of others, not always unique
 
-    //Animations (Core)
-    protected Map<String, SpriteSheet> animations;
-    
-    //Gameplay Statistics
-    protected int eHealth; // The entity's maximum health
-    protected int eMana; // The entity's maximum mana
-    protected List<Trait> traits; // A series of 3 letter keywords, assist in determining derived stats
-    
-    /*
-     * Initializes a being into...being. This is overridden in the PC, NPC, Monster classes
-     *
-     * Assumption: All three parameters are properly initialized
-     * @param id The entity ID
-     * @param name Entity handle
-     * @param sID The spriteSheet ID
-     */
-    abstract void createEntity(long id, String name, long sID, int hp, int mp, String [] traits);
+   //Animations (Core)
+   protected Map<String, SpriteSheet> animations;
 
-    /**
-    * Writes an Entity to a JSON object
-    */
-    abstract void writeToJson();
+   //Gameplay Statistics
+   protected int eHealth; // The entity's maximum health
+   protected int eMana; // The entity's maximum mana
+   protected List<Trait> traits; // A series of 3 letter keywords, assist in determining derived stats
+
+   abstract void createEntity(String name);
 }
